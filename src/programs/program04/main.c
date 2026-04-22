26d38c5865b14a08d4a1d2b6f40c0cf807dfb64b
whausleitner <58576046+whausleitner@users.noreply.github.com>
2026-04-22 20:38:35 +0100

    Update main.c

--- src/programs/program04/main.c
+++ src/programs/program04/main.c
@@ -1,5 +1,18 @@
+/// \file
+/// \author
+/// \date
+
+#include <inttypes.h>
 #include <stdio.h>
 
-int main(int argc, char ** argv) {
-// write your solution here
+// TODO: your code goes here
+
+int main(void) {
+  double leg_a = 0, leg_b = 0;
+
+  scanf("%lf", &leg_a);
+
+  printf("%f", compute_hypotenuse(leg_a, leg_b));
+
+  return 0;
 }
