74b4e51a3b049c1777719f583f591285a104d91f
whausleitner <58576046+whausleitner@users.noreply.github.com>
2026-04-22 20:47:10 +0100

    Update main.c

--- src/programs/program01/main.c
+++ src/programs/program01/main.c
@@ -1,5 +1,11 @@
+/// \file
+/// \author
+/// \date
+
+#include <inttypes.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  // write your solution here
+   // write your solution here
+
 }
