865315a11dc943b98507a5043f9e2ac1f0676654
whausleitner <58576046+whausleitner@users.noreply.github.com>
2026-04-22 20:34:02 +0100

    Update main.c

--- src/programs/program03/main.c
+++ src/programs/program03/main.c
@@ -1,5 +1,18 @@
+/// \file
+/// \author
+/// \date
+
+#include <inttypes.h>
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-  // write your solution here
+// TODO: your code goes here
+
+int main(void) {
+  double temp_celsius = 0;
+
+  scanf("%lf", &temp_celsius);
+
+  printf("%f", Celsius2Fahrenheit(temp_celsius));
+
+  return 0;
 }
